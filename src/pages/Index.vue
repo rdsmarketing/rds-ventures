af8256c314139848061ffbe2c8bda88dd5f4458c
rdsmarketing <riley@rds.marketing>
2019-12-22 19:18:32 -0500

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <Layout>
+
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1>Welcome To RDS-Ventures</h1>
+
+    <p>
+      We are a proffesional Technology Company with over 2 decades of Industry expierence among our C-Level executives.
+    </p>
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
+      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    </p>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
---
+++ src/pages/blog.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <div>
+        <h1>Welcome to our Blog!</h1>
+        <p>We update everyday so be sure to subscribe!</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        metaInfo: {
+    title: 'Our Blog!'
+  }
+    }
+</script>
+
+<style scoped>
+
+</style>
